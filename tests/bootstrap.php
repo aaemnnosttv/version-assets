cdec9cd3e5b987b430cfbf1c982f3ccc5ddb819d
Evan Mattson <me@aaemnnost.tv>
2017-09-17 10:06:40 +0300

    inline unnecessary constant

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,9 @@
 <?php
-define('WP_TESTS_DIR', getenv('WP_TESTS_DIR'));
 
-require WP_TESTS_DIR . '/includes/functions.php';
+require getenv('WP_TESTS_DIR') . '/includes/functions.php';
 
 tests_add_filter('muplugins_loaded', function () {
     require_once dirname(__DIR__) . '/version-assets.php';
 });
 
-require WP_TESTS_DIR . '/includes/bootstrap.php';
+require getenv('WP_TESTS_DIR') . '/includes/bootstrap.php';
