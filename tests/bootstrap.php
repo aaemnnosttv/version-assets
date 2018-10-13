4fde70b27ba280e574a2e54acbe0ab5aec1b944b
Evan Mattson <me@aaemnnost.tv>
2018-10-13 12:54:42 +0300

    add wp-phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
-require getenv('WP_TESTS_DIR') . '/includes/functions.php';
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
-tests_add_filter('muplugins_loaded', function () {
+tests_add_filter( 'muplugins_loaded', function() {
     require_once dirname(__DIR__) . '/version-assets.php';
-});
+} );
 
-require getenv('WP_TESTS_DIR') . '/includes/bootstrap.php';
+require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
