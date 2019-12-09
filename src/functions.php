034074d086188ac751819e2afd7ff17084d0e52c
Evan Mattson <me@aaemnnost.tv>
2019-12-09 13:44:31 +0200

    refactor out added functions

--- src/functions.php
+++ src/functions.php
@@ -32,67 +32,27 @@ function get_local_path($src)
         return false;
     }
 
-    // Core assets are all registered relative to ABSPATH,
-    // so we must rewrite the src if this is not the same as the web root to get the correct path.
-    if (is_core_src($src)) {
-        $src = rewrite_core_src($src);
+    /**
+     * Filter the path used for the web root.
+     */
+    $web_root = apply_filters('version_assets/web_root', dirname(WP_CONTENT_DIR));
+    $web_root = untrailingslashit($web_root);
+
+    // Core assets are all registered using relative srcs, from ABSPATH (not web root),
+    // so we must rewrite the src if ABSPATH is not the same as the web root to get the correct path.
+    if (preg_match('#^/wp-(admin|includes)/#', $src) && $web_root !== untrailingslashit(ABSPATH)) {
+        $preg_quoted_web_root = preg_quote($web_root, '#');
+        $wordpress_path = preg_replace("#^$preg_quoted_web_root#", '', untrailingslashit(ABSPATH));
+        $src = path_join($wordpress_path, ltrim($src, '/'));
     }
 
-    $file_path = path_join(guess_web_root(), ltrim(parse_url($src, PHP_URL_PATH), '/\\'));
+    $file_path = path_join($web_root, ltrim(parse_url($src, PHP_URL_PATH), '/\\'));
 
     if (realpath($file_path)) {
         return $file_path;
     }
 
     return false;
-}
-
-/**
- * Get the absolute path to the web root directory.
- *
- * The web root is inferred as the parent of the content directory by default.
- *
- * @return string
- */
-function guess_web_root() {
-    /**
-     * Filter the path used for the web root.
-     */
-    $path = apply_filters('version_assets/web_root', dirname(WP_CONTENT_DIR));
-
-    return untrailingslashit($path);
-}
-
-/**
- * Checks if the given src is for a core asset.
- *
- * @param string $src Asset src to check.
- *
- * @return bool
- */
-function is_core_src($src) {
-    return (
-        // Core assets are all registered using relative srcs, from ABSPATH (not web root).
-        is_relative_url($src)
-        // Only rewrite the src if core is not installed in the web root.
-        && guess_web_root() !== untrailingslashit(ABSPATH)
-        // Check that the src is for a core asset.
-        && preg_match('#^/wp-(admin|includes)/#', $src)
-    );
-}
-
-/**
- * Rewrites a core src to be relative to the web root.
- *
- * @param string $src Asset src to rewrite.
- *
- * @return string
- */
-function rewrite_core_src($src) {
-    $preg_quoted_web_root = preg_quote(guess_web_root(), '#');
-    $wordpress_path = preg_replace("#^$preg_quoted_web_root#", '', untrailingslashit(ABSPATH));
-
-    return path_join($wordpress_path, ltrim($src, '/'));
 }
 
 /**
